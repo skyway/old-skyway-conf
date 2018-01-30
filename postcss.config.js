0a2726fe743211ce73457c56d29f3b621955ec3f
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-01-30 15:26:07 +0900

    Enable to postcss multiple entries

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,17 @@
+const path = require('path');
+
+const rootPath = path.resolve('');
+
 module.exports = {
+  context: rootPath,
+  entry: {
+    index: './src/index/main.css',
+    room: './src/room/main.css',
+  },
+  output: {
+    path: `${rootPath}/public`,
+    filename: '[name].bundle.css',
+  },
   plugins: {
     'postcss-import': {},
     'postcss-nested': {},
