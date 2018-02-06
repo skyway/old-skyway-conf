8627f2ded6755cad0e50f5e9f012a6fcd54f83db
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-02-06 16:15:33 +0900

    Refactor popup

--- src/conf/component/popup.jsx
+++ src/conf/component/popup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
-const Popup = ({ children }) => <div className="Popup">{children}</div>;
+const Popup = ({ isVisible, children }) =>
+  isVisible ? <div className="Popup">{children}</div> : null;
 
 export default observer(Popup);
--- src/conf/container/setting.jsx
+++ src/conf/container/setting.jsx
@@ -6,31 +6,30 @@ import SettingVideo from '../component/setting-video';
 import DeviceSelector from '../component/device-selector';
 import NameEdit from '../component/name-edit';
 
-const Setting = ({ room, ui, user, action }) =>
-  ui.isSettingOpen ? (
-    <Popup>
-      <div className="L-Setting">
-        <div className="L-Setting_Video">
-          <SettingVideo room={room} />
-        </div>
-
-        <DeviceSelector user={user} action={action} />
-
-        <NameEdit user={user} action={action} />
-
-        <div className="L-Setting_Finish">
-          <button
-            onClick={() => {
-              ui.isRoomJoin
-                ? action.$update('ui.isSettingOpen', false)
-                : action.onClickJoinRoom();
-            }}
-          >
-            設定完了
-          </button>
-        </div>
+const Setting = ({ room, ui, user, action }) => (
+  <Popup isVisible={ui.isSettingOpen}>
+    <div className="L-Setting">
+      <div className="L-Setting_Video">
+        <SettingVideo room={room} />
       </div>
-    </Popup>
-  ) : null;
+
+      <DeviceSelector user={user} action={action} />
+
+      <NameEdit user={user} action={action} />
+
+      <div className="L-Setting_Finish">
+        <button
+          onClick={() => {
+            ui.isRoomJoin
+              ? action.$update('ui.isSettingOpen', false)
+              : action.onClickJoinRoom();
+          }}
+        >
+          設定完了
+        </button>
+      </div>
+    </div>
+  </Popup>
+);
 
 export default inject('room', 'ui', 'user', 'action')(observer(Setting));
