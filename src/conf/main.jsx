c8cdedd69a3c6d3fa46815febdb719986eda4c93
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-02-15 12:24:55 +0900

    Redirect if not supported

--- src/conf/main.jsx
+++ src/conf/main.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 
+import util from '../shared/util';
 import ConfStore from './store';
 import ConfAction from './action';
 import ConfApp from './app';
 
-const store = new ConfStore();
-const action = new ConfAction(store);
+const ua = navigator.userAgent;
+if (util.supportOs(ua) && util.supportBrowser(ua)) {
+  const store = new ConfStore();
+  const action = new ConfAction(store);
 
-// TODO: for debug
-window.store = store;
-window.action = action;
-
-const [, roomType, roomName] = location.hash.split('/');
-action.onLoad({ roomType, roomName });
-ReactDOM.render(
-  <Provider action={action} {...store}>
-    <ConfApp />
-  </Provider>,
-  document.getElementById('app-root')
-);
+  const [, roomType, roomName] = location.hash.split('/');
+  action.onLoad({ roomType, roomName });
+  ReactDOM.render(
+    <Provider action={action} {...store}>
+      <ConfApp />
+    </Provider>,
+    document.getElementById('app-root')
+  );
+} else {
+  location.href = '/not_supported.html';
+}
--- src/shared/util.js
+++ src/shared/util.js
@@ -9,7 +9,49 @@ function isValidRoomName(name) {
   return /^[0-9a-z_-]{4,32}$/.test(name);
 }
 
+function supportOs(ua) {
+  switch (true) {
+    case /Android/.test(ua):
+      return false;
+    case /iPhone|iPad|iPod/.test(ua):
+      return false;
+    case /Windows/.test(ua):
+      return true;
+    case /Mac OS X/.test(ua):
+      return true;
+    case /CrOS/.test(ua):
+      return false;
+    case /Firefox/.test(ua):
+      return false;
+    default:
+      return false;
+  }
+}
+
+function supportBrowser(ua) {
+  switch (true) {
+    case /CriOS/.test(ua):
+      return false;
+    case /Edge/.test(ua):
+      return false;
+    case /Chrome/.test(ua):
+      return true;
+    case /Firefox/.test(ua):
+      return true;
+    case /MSIE|Trident/.test(ua):
+      return false;
+    case /Safari\//.test(ua):
+      return false;
+    case /AppleWebKit/.test(ua):
+      return false;
+    default:
+      return false;
+  }
+}
+
 export default {
   isValidRoomType,
   isValidRoomName,
+  supportOs,
+  supportBrowser,
 };
