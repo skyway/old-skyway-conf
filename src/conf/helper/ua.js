334b94a815f80650139c734de6d45fe7d02d5c06
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-02-21 14:58:32 +0900

    Check browser and room type

---
+++ src/conf/helper/ua.js
@@ -0,0 +1,44 @@
+function getOsName(ua) {
+  switch (true) {
+    case /Android/.test(ua):
+      return 'Android';
+    case /iPhone|iPad|iPod/.test(ua):
+      return 'iOS';
+    case /Windows/.test(ua):
+      return 'Windows';
+    case /Mac OS X/.test(ua):
+      return 'Mac';
+    case /CrOS/.test(ua):
+      return 'CromeOS';
+    case /Firefox/.test(ua):
+      return 'FirefoxOS';
+    default:
+      return '';
+  }
+}
+
+function getBrowserName(ua) {
+  switch (true) {
+    case /CriOS/.test(ua):
+      return 'iOSChrome';
+    case /Edge/.test(ua):
+      return 'Edge';
+    case /Chrome/.test(ua):
+      return 'Chrome';
+    case /Firefox/.test(ua):
+      return 'Firefox';
+    case /MSIE|Trident/.test(ua):
+      return 'IE';
+    case /Safari\//.test(ua):
+      return 'Safari';
+    case /AppleWebKit/.test(ua):
+      return 'Webkit';
+    default:
+      return '';
+  }
+}
+
+export default {
+  getOsName,
+  getBrowserName,
+};
--- src/conf/main.jsx
+++ src/conf/main.jsx
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 
-import util from '../shared/util';
+import ua from './helper/ua';
 import ConfStore from './store';
 import ConfAction from './action';
 import ConfApp from './app';
 
-const ua = navigator.userAgent;
-if (util.isSupportedEnv(ua)) {
+const [, roomType, roomName] = location.hash.split('/');
+
+const uaString = navigator.userAgent;
+const browser = ua.getBrowserName(uaString);
+const isSupported =
+  ['Windows', 'Mac'].includes(ua.getOsName(uaString)) &&
+  ['Chrome', 'Firefox', 'Safari'].includes(browser);
+if (isSupported === false) {
+  location.href = './not_supported.html';
+} else if (browser === 'Safari' && roomType !== 'sfu') {
+  location.href = './not_supported.html';
+} else {
   const store = new ConfStore();
   const action = new ConfAction(store);
 
+  action.onLoad({ roomType, roomName });
+  ReactDOM.render(
+    <Provider action={action} {...store}>
+      <ConfApp />
+    </Provider>,
+    document.getElementById('app-root')
+  );
+
+  // for debug
   if (process.env.NODE_ENV !== 'production') {
     window.store = store;
     window.action = action;
@@ -29,15 +48,4 @@ if (util.isSupportedEnv(ua)) {
         };
       });
   }
-
-  const [, roomType, roomName] = location.hash.split('/');
-  action.onLoad({ roomType, roomName });
-  ReactDOM.render(
-    <Provider action={action} {...store}>
-      <ConfApp />
-    </Provider>,
-    document.getElementById('app-root')
-  );
-} else {
-  location.href = './not_supported.html';
 }
