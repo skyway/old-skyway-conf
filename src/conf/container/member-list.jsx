7aada56420456b6874013fedf24ddd95b2727446
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-08 15:44:12 +0900

    Show arrow UI to express scrollable

--- src/conf/container/member-list.jsx
+++ src/conf/container/member-list.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 
+import MemberList from '../component/member-list';
 import MemberVideoLocal from '../component/member-video/local';
 import MemberVideoRemotes from '../component/member-video/remotes';
 import MemberVideoInvite from '../component/member-video/invite';
 
-const MemberList = ({ ui, room, user, action }) => (
-  <div className="L-MemberList">
+const MemberListC = ({ ui, room, user, action }) => (
+  <MemberList room={room}>
     <MemberVideoLocal ui={ui} room={room} user={user} action={action} />
     <MemberVideoRemotes room={room} action={action} />
     <MemberVideoInvite action={action} />
-  </div>
+  </MemberList>
 );
 
-export default inject('ui', 'room', 'user', 'action')(observer(MemberList));
+export default inject('ui', 'room', 'user', 'action')(observer(MemberListC));
