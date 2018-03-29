ab7480ac12ee7596dc44f047cce2d3a092d96f60
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-29 14:35:28 +0900

    Check env too

--- src/conf_mobile/main.jsx
+++ src/conf_mobile/main.jsx
@@ -5,36 +5,58 @@ import { Provider } from 'mobx-react';
 import ConfStore from './store';
 import ConfAction from './action';
 import ConfApp from './app';
+import bom from '../shared/util/bom';
 
-const [, roomType, roomName] = location.hash.split('/');
-const store = new ConfStore();
-const action = new ConfAction(store);
+(function() {
+  const [, roomType, roomName] = location.hash.split('/');
+  const os = bom.getOsName(navigator.userAgent);
+  const browser = bom.getBrowserName(navigator.userAgent);
 
-action.onLoad({ roomType, roomName });
-ReactDOM.render(
-  <Provider action={action} {...store}>
-    <ConfApp />
-  </Provider>,
-  document.getElementById('app-root')
-);
+  if (
+    (['Windows', 'Mac', 'iOS', 'Android'].includes(os) &&
+      ['Chrome', 'Firefox', 'Safari'].includes(browser)) === false ||
+    // allow Safari to enter mesh room only
+    (browser === 'Safari' && roomType !== 'mesh')
+  ) {
+    location.href = './not_supported.html';
+    return;
+  }
 
-window.addEventListener('hashchange', () => location.reload(true));
+  // if not mobile, redirect
+  if (['iOS', 'Android'].includes(os) === false) {
+    location.href = `./conf.html${location.hash}`;
+    return;
+  }
 
-// for debug
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-  window.action = action;
+  const store = new ConfStore();
+  const action = new ConfAction(store);
 
-  const proto = Object.getPrototypeOf(action);
-  Object.getOwnPropertyNames(proto)
-    .filter(name => {
-      return name !== 'constructor' && typeof proto[name] === 'function';
-    })
-    .forEach(key => {
-      const origMethod = action[key];
-      action[key] = (...args) => {
-        console.warn(`[action] ${key}`, ...args);
-        origMethod.call(action, ...args);
-      };
-    });
-}
+  action.onLoad({ roomType, roomName });
+  ReactDOM.render(
+    <Provider action={action} {...store}>
+      <ConfApp />
+    </Provider>,
+    document.getElementById('app-root')
+  );
+
+  window.addEventListener('hashchange', () => location.reload(true));
+
+  // for debug
+  if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+    window.action = action;
+
+    const proto = Object.getPrototypeOf(action);
+    Object.getOwnPropertyNames(proto)
+      .filter(name => {
+        return name !== 'constructor' && typeof proto[name] === 'function';
+      })
+      .forEach(key => {
+        const origMethod = action[key];
+        action[key] = (...args) => {
+          console.warn(`[action] ${key}`, ...args);
+          origMethod.call(action, ...args);
+        };
+      });
+  }
+})();
