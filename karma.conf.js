67d67629c751eb12d3a5f794976d4bb1d6efbd99
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-30 16:24:26 +0900

    Remove nonused prop

--- karma.conf.js
+++ karma.conf.js
@@ -21,12 +21,7 @@ module.exports = function(config) {
     },
     singleRun: true,
     reporters: ['mocha'],
-    port: 9876, // karma web server port
-    colors: true,
-    logLevel: config.LOG_INFO,
+    port: 9876,
     browsers: ['ChromeHeadless'],
-    autoWatch: false,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
-    concurrency: Infinity,
   });
 };
