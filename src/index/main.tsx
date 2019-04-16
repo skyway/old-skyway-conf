33d28eae3f7ac8ecda0f997eb74c6fceab22deee
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-04-16 14:40:35 +0900

    Use emotion for styling

--- src/index/main.tsx
+++ src/index/main.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { version } from '../../package.json';
+import { Global, css } from '@emotion/core';
 
-const App = () => <div>v{version}</div>;
+const globalStyle = css({
+  'html, body': {
+    padding: 0,
+    margin: 0,
+  },
+});
 
-render(<App />, document.getElementById('app-root'));
+const style = css({
+  backgroundColor: 'orange',
+  boxSizing: 'border-box',
+});
+
+const App = () => <div css={style}>v{version}</div>;
+
+render(
+  <>
+    <Global styles={globalStyle} />
+    <App />
+  </>,
+  document.getElementById('app-root'),
+);
