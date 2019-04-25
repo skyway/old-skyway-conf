755322bd1473c7f2824959c3193781f5804a79f4
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-04-25 20:31:48 +0900

    App as error boundary

--- src/conf/app.tsx
+++ src/conf/app.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
-import { FunctionComponent } from "react";
 import Bootstrap from "./containers/bootstrap";
 import Settings from "./containers/settings";
+import ErrorDetail from "./components/error-detail";
 
-const App: FunctionComponent<{}> = () => {
-  return (
-    <Bootstrap>
-      <Settings />
-      <div>xxx</div>
-    </Bootstrap>
-  );
-};
+interface State {
+  err: Error | null;
+}
+class App extends React.Component<never, State> {
+  constructor(props: never, state: State) {
+    super(props, state);
+    this.state = { err: null };
+  }
+
+  render() {
+    if (this.state.err !== null) {
+      return <ErrorDetail error={this.state.err} />;
+    }
+
+    return (
+      <Bootstrap>
+        <Settings />
+        <div>xxx</div>
+      </Bootstrap>
+    );
+  }
+
+  componentDidCatch(err: Error) {
+    this.setState({ err });
+  }
+}
 
 export default App;
