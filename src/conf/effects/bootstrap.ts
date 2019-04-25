db24221100105566ae6f384eda0c386577b7b5f5
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-04-25 20:07:55 +0900

    Revert "Throw everywhere"
    This reverts commit eef7ebb0039b34614697197c73ef86c0a8395f9f.

--- src/conf/effects/bootstrap.ts
+++ src/conf/effects/bootstrap.ts
@@ -3,33 +3,38 @@ import { toJS } from "mobx";
 import debug from "debug";
 import { isValidRoomName, isValidRoomType } from "../../shared/validate";
 import { getUserDevices } from "../utils//webrtc";
+import { UserDevices } from "../utils/types";
 import RootStore from "../stores";
 
 const log = debug("effect:bootstrap");
 
-export const checkRoomSetting = (): EffectCallback => () => {
+export const checkRoomSetting = ({ ui }: RootStore): EffectCallback => () => {
   log("checkRoomSetting()");
   const [, roomType, roomName] = location.hash.split("/");
 
   if (!(isValidRoomType(roomType) && isValidRoomName(roomName))) {
-    throw new Error("Invalid room type and/or room name!");
+    ui.showError(new Error("Invalid room type and/or room name."));
   }
 
   log(`room: ${roomType}/${roomName}`);
 };
 
 export const ensureAudioDevice = ({
+  ui,
   media
 }: RootStore): EffectCallback => () => {
   log("ensureAudioDevice()");
 
   (async () => {
-    const devices = await getUserDevices();
+    const devices = (await getUserDevices().catch(err =>
+      ui.showError(err)
+    )) as UserDevices;
     media.updateDevices(devices);
 
     const hasAudioDevice = devices;
+
     if (!hasAudioDevice) {
-      throw new Error("At least one audio input device needed!");
+      ui.showError(new Error("At least one audio input device needed!"));
     }
 
     log("devices", { ...devices });
--- src/conf/stores/ui.ts
+++ src/conf/stores/ui.ts
@@ -1,14 +1,26 @@
-import { decorate, observable } from "mobx";
+import { decorate, observable, action } from "mobx";
+import debug from "debug";
+
+const log = debug("store:ui");
 
 class UiStore {
+  error: Error | null;
   isSettingsOpen: boolean;
 
   constructor() {
+    this.error = null;
     this.isSettingsOpen = true;
+  }
+
+  showError(err: Error) {
+    log("error", err);
+    this.error = err;
   }
 }
 decorate(UiStore, {
-  isSettingsOpen: observable
+  error: observable.ref,
+  isSettingsOpen: observable,
+  showError: action
 });
 
 export default UiStore;
