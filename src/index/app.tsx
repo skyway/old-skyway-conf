43655e1920eb459d464fc20dd3599f3fa6fbb6d5
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-05 21:00:50 +0900

    Split layout component

--- src/index/app.tsx
+++ src/index/app.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { FunctionComponent } from "react";
-import { css } from "@emotion/core";
-import { globalColors } from "../shared/global-style";
 import { RoomInit } from "./utils/types";
+import Layout from "./components/layout";
 import RoomCreate from "./components/room-create";
 
 const App: FunctionComponent<{}> = () => {
@@ -13,56 +12,10 @@ const App: FunctionComponent<{}> = () => {
   };
 
   return (
-    <div css={wrapperStyle}>
-      <h1 css={headStyle}>SkyWay Conference</h1>
-      <p css={pStyle}>Video conference on web browser</p>
-
+    <Layout>
       <RoomCreate onSubmit={room => effects.enterConference(room)} />
-
-      <p css={pStyle}>
-        No registration or installs! Just create a room and share the link.
-      </p>
-      <a
-        css={logoStyle}
-        href="https://webrtc.ecl.ntt.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="./images/index/icon-skyway.svg" alt="SkyWay" />
-      </a>
-    </div>
+    </Layout>
   );
 };
 
 export default App;
-
-const wrapperStyle = css({
-  backgroundImage: "url(./images/index/bg.jpg)",
-  backgroundSize: "cover",
-  height: "100vh",
-  position: "relative",
-  textAlign: "center",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center"
-});
-
-const headStyle = css({
-  margin: 0,
-  color: globalColors.lightblue,
-  fontSize: "3rem"
-});
-
-const pStyle = css({
-  color: globalColors.lightblue,
-  fontSize: "1.2rem"
-});
-
-const logoStyle = css({
-  position: "absolute",
-  bottom: 10,
-  right: 10,
-  "& > img": {
-    height: 40
-  }
-});
---
+++ src/index/components/layout.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import { FunctionComponent, ReactNode } from "react";
+import { css } from "@emotion/core";
+import { globalColors } from "../../shared/global-style";
+
+interface Props {
+  children: ReactNode;
+}
+const Layout: FunctionComponent<Props> = ({ children }: Props) => (
+  <div css={wrapperStyle}>
+    <h1 css={headStyle}>SkyWay Conference</h1>
+    <p css={pStyle}>Video conference on web browser</p>
+
+    {children}
+
+    <p css={pStyle}>
+      No registration or installs! Just create a room and share the link.
+    </p>
+    <a
+      css={logoStyle}
+      href="https://webrtc.ecl.ntt.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="./images/index/icon-skyway.svg" alt="SkyWay" />
+    </a>
+  </div>
+);
+
+export default Layout;
+
+const wrapperStyle = css({
+  backgroundImage: "url(./images/index/bg.jpg)",
+  backgroundSize: "cover",
+  height: "100vh",
+  position: "relative",
+  textAlign: "center",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center"
+});
+
+const headStyle = css({
+  margin: 0,
+  color: globalColors.lightblue,
+  fontSize: "3rem"
+});
+
+const pStyle = css({
+  color: globalColors.lightblue,
+  fontSize: "1.2rem"
+});
+
+const logoStyle = css({
+  position: "absolute",
+  bottom: 10,
+  right: 10,
+  "& > img": {
+    height: 40
+  }
+});
