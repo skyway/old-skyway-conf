8b5dde4ed7c47a699dfb163c2ee1cf955d3736c8
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-10 18:32:31 +0900

    Remove TODO line

--- src/conf/components/settings-layout.tsx
+++ src/conf/components/settings-layout.tsx
@@ -12,7 +12,6 @@ import SettingsStreamController from "./settings-stream-controller";
 
 interface Props {
   stream: MediaStream;
-  // TODO: default..?
   defaultDispName: string;
   browser: ClientBrowser;
   hasGetDisplayMedia: boolean;
