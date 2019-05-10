4d8ffafdada32a85d057e995ca1c923660732eb1
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-10 11:20:27 +0900

    Fix pinned style

--- src/conf/components/remote-streams-layout.tsx
+++ src/conf/components/remote-streams-layout.tsx
@@ -4,8 +4,9 @@ import { css } from "@emotion/core";
 import { globalColors } from "../../shared/global-style";
 import { RoomStream, RoomStat } from "../utils/types";
 import { rightMenuWidth, rightMenuTogglerHeight } from "../utils/style";
-import RemoteStreamController from "../components/remote-stream-controller";
-import Video from "../components/video";
+import RemoteStreamController from "./remote-stream-controller";
+import Video from "./video";
+import { Icon } from "./icon";
 
 interface Props {
   streams: [string, RoomStream][];
@@ -24,22 +25,24 @@ const RemoteStreamsLayout: FunctionComponent<Props> = ({
     {streams.map(([peerId, stream]) => {
       const entry = stats.find(([id]) => id === peerId);
       const stat = entry ? entry[1] : null;
+      const isPinned = peerId === pinnedId;
       return (
         <div
           key={peerId}
           css={wrapperStyle}
           onClick={() => onClickSetPinned(peerId)}
         >
+          {isPinned ? (
+            <div css={pinnedStyle}>
+              <Icon name="present_to_all" />
+            </div>
+          ) : null}
           <div css={controllerStyle}>
             {stat !== null ? (
               <RemoteStreamController {...stat} stream={stream} />
             ) : null}
           </div>
-          <div
-            css={
-              peerId === pinnedId ? [videoStyle, pinnedVideoStyle] : videoStyle
-            }
-          >
+          <div css={videoStyle}>
             <Video stream={stream} />
           </div>
         </div>
@@ -51,7 +54,26 @@ const RemoteStreamsLayout: FunctionComponent<Props> = ({
 export default RemoteStreamsLayout;
 
 const wrapperStyle = css({
-  position: "relative"
+  position: "relative",
+  cursor: "pointer"
+});
+
+const pinnedStyle = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 1,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "rgba(0, 0, 0, .8)",
+
+  "& > *": {
+    fontSize: "2rem",
+    color: globalColors.white
+  }
 });
 
 const headStyle = css({
@@ -67,19 +89,10 @@ const controllerStyle = css({
   left: 0,
   right: 0,
   bottom: 0,
-  zIndex: 1
+  zIndex: 10
 });
 
 const videoStyle = css({
-  position: "relative",
   // 4:3
-  height: (rightMenuWidth / 4) * 3,
-  boxSizing: "border-box",
-  cursor: "pointer"
-});
-
-const pinnedVideoStyle = css({
-  border: `2px solid ${globalColors.lightblue}`,
-  cursor: "default",
-  pointerEvents: "none"
+  height: (rightMenuWidth / 4) * 3
 });
