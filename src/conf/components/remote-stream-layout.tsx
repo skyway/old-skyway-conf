e9c0d701cf40ba4106148ad6354bfd3fc0ad6308
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-13 15:55:35 +0900

    Refactor pinned UI

--- src/conf/components/remote-stream-layout.tsx
+++ src/conf/components/remote-stream-layout.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { FunctionComponent } from "react";
 import { css } from "@emotion/core";
 import { globalColors } from "../../shared/global-style";
@@ -6,7 +7,7 @@ import { RoomStream, RoomStat } from "../utils/types";
 import { rightMenuWidth } from "../utils/style";
 import Video from "./video";
 import StreamController from "./stream-controller";
-import { Icon } from "./icon";
+import { Icon, IconButton } from "./icon";
 import VADetector from "./va-detector";
 
 interface Props {
@@ -20,43 +21,54 @@ const RemoteStreamLayout: FunctionComponent<Props> = ({
   stat,
   isPinned,
   onClickSetPinned
-}: Props) => (
-  <div css={wrapperStyle} onClick={onClickSetPinned}>
-    {isPinned ? (
-      <div css={pinnedStyle}>
-        <Icon name="present_to_all" />
-      </div>
-    ) : null}
-    <div css={videoStyle}>
-      <Video stream={stream} />
-      <div css={controllerStyle}>
-        {stat !== null ? (
-          <StreamController
-            displayName={stat.displayName}
-            browser={stat.browser}
-            controllers={
-              <>
-                {stat.isVideoDisabled ? null : (
-                  <Icon
-                    name={stat.isVideoMuted ? "videocam_off" : "videocam"}
-                  />
-                )}
-                <Icon name={stat.isAudioMuted ? "mic_off" : "mic"} />
-                <VADetector stream={stream} />
-              </>
-            }
+}: Props) => {
+  const [isHover, setHover] = useState(false);
+  const isVideoDisabled = stat && stat.isVideoDisabled ? true : false;
+
+  return (
+    <div
+      css={wrapperStyle}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {!isVideoDisabled && isHover ? (
+        <div css={pinnedStyle}>
+          <IconButton
+            name={isPinned ? "cancel_presentation" : "present_to_all"}
+            onClick={onClickSetPinned}
           />
-        ) : null}
+        </div>
+      ) : null}
+      <div css={videoStyle}>
+        <Video stream={stream} />
+        <div css={controllerStyle}>
+          {stat !== null ? (
+            <StreamController
+              displayName={stat.displayName}
+              browser={stat.browser}
+              controllers={
+                <>
+                  {stat.isVideoDisabled ? null : (
+                    <Icon
+                      name={stat.isVideoMuted ? "videocam_off" : "videocam"}
+                    />
+                  )}
+                  <Icon name={stat.isAudioMuted ? "mic_off" : "mic"} />
+                  <VADetector stream={stream} />
+                </>
+              }
+            />
+          ) : null}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RemoteStreamLayout;
 
 const wrapperStyle = css({
-  position: "relative",
-  cursor: "pointer"
+  position: "relative"
 });
 
 const pinnedStyle = css({
@@ -71,8 +83,8 @@ const pinnedStyle = css({
   alignItems: "center",
   backgroundColor: "rgba(0, 0, 0, .8)",
 
-  "& > *": {
-    fontSize: "2rem",
+  "& > button": {
+    transform: "scale(2)",
     color: globalColors.white
   }
 });
