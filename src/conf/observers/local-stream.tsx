4439630b634772ba6b5267505d666779807b92a0
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-13 17:08:35 +0900

    Cosme

--- src/conf/observers/local-stream.tsx
+++ src/conf/observers/local-stream.tsx
@@ -37,7 +37,7 @@ const LocalStream: FunctionComponent<{}> = () => {
             isAudioTrackMuted={media.isAudioTrackMuted}
             onClickToggleAudioMuted={onClickToggleAudioMuted}
             onClickToggleVideoMuted={onClickToggleVideoMuted}
-          onClickCastVideo={onClickCastVideo}
+            onClickCastVideo={onClickCastVideo}
             onClickOpenSettings={onClickOpenSettings}
           />
         );
