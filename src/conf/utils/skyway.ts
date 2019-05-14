452e7910f394cb091b6ecfe3bcfcaeafbb8faa48
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-14 15:47:49 +0900

    Restore key

--- src/conf/utils/skyway.ts
+++ src/conf/utils/skyway.ts
@@ -4,9 +4,7 @@ export const initPeer = (): Promise<Peer> => {
   return new Promise((resolve, reject) => {
     // @ts-ignore: import via cdn for auto-update
     const peer = new window.Peer({
-      // TODO: dev key
-      key: "adbec654-2931-42c3-a073-7450b1c2def1",
-      // key: "03ff6219-b58f-4310-9484-e9108e859cdd",
+      key: "03ff6219-b58f-4310-9484-e9108e859cdd",
       debug: 2
     }) as Peer;
 
