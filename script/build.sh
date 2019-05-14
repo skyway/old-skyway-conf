96668d6549de2063aa8f7030003b0b23e7b0be80
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-14 17:23:23 +0900

    Enhance build script

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,8 @@
-npm run build
-git add .
-git commit -m Build
+if [[ "$1" =~ ^(major|minor|patch)$ ]]; then
+  version=$(npm --no-git-tag-version version $1)
+  npm run build
+  git add .
+  git commit -m "Build: $version"
+else
+  echo "Build version is missing..."
+fi
